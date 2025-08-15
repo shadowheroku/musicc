0b0923659f46279d4e9589527986918911ce3adb
shadowheroku <shadowheroku@gmail.com>
2025-08-15 14:01:32 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# musicc
+<h2 align="center">
+    ──「 Yumeko IS A MODULAR AND MUSIC BOT WITH ANIME THEME 」──
+</h2>
+
+<p align="center">
+  <img src="https://telegra.ph/file/900499565371726a3f256.jpg">
+</p>
+
+<p align="center">
+<a href="https://t.me/Yumeko_ProBot"> <img src="https://github.com/Team-Infamous/YumekoBot/blob/main/Yumeko/resources/siesta-robot.svg" alt="Yumeko on Telegram" /> </a></br>
+<a href="https://github.com/Team-Infamous/Yumeko/blob/main/LICENSE"> <img src="https://img.shields.io/badge/License-GPLv3-blueviolet?style=for-the-badge" alt="License" /> </a>
+<a href="https://makeapullrequest.com"> <img src="https://img.shields.io/badge/PRs-Welcome-yellow?style=for-the-badge" alt="PRs" /></a></br>
+<a href="https://www.python.org/"> <img src="https://img.shields.io/badge/Made%20With-Python-orange?style=for-the-badge&logo=python" alt="Python" /> </a>
+<a href="https://GitHub.com/Team-Infamous/Yumeko"> <img src="https://img.shields.io/badge/Maintained-Yes-lightgrey?style=for-the-badge" alt="Maintenance" /> </a>
+<a href="https://docs.sqlalchemy.org/en/14/"> <img src="https://img.shields.io/badge/SQL%20Alchemy-1.4.29-green?style=for-the-badge" alt="SQL Alchemy" /> </a><br>
+<a href="https://python-telegram-bot.org"> <img src="https://img.shields.io/badge/PTB-12.8-white?style=for-the-badge&logo=github" alt="Python Telegram Bot" /> </a>
+<a href="https://docs.telethon.dev"> <img src="https://img.shields.io/badge/Telethon-1.23.0-red?style=for-the-badge&logo=github" alt="Telethon" /> </a>
+<a href="https://docs.python.org"> <img src="https://img.shields.io/badge/Python-2.8.1-purple?style=for-the-badge&logo=python" alt="Python Version" /> </a>
+</p>
+
+<p align="center">
+  <img src="https://telegra.ph/file/5835dce546af117c637d4.jpg">
+</p>
+
+<h2 align="center">
+    ──「 DEPLOY ON HEROKU 」──
+</h2>
+
+<p align="center"><a href="https://heroku.com/deploy?template=https://github.com/Team-Infamous/Yumeko"> <img src="https://img.shields.io/badge/Deploy%20To%20Heroku-purple?style=for-the-badge&logo=heroku" width="220" height="38.45"/></a></p>
+
+
+<h2 align="center">
+    ──「 SUPPORT 」──
+</h2>
+
+<p align="center">
+<a href="https://t.me/Yumeko_ProBot"> <img src="https://img.shields.io/badge/Yumeko-User-green?style=for-the-badge&logo=telegram" alt="Yumeko on Telegram" /> </a>
+<a href="https://t.me/Raiden_Support"> <img src="https://img.shields.io/badge/Support-Chat-green?style=for-the-badge&logo=telegram" alt="Support Chat" /> </a>
+<a href="https://t.me/Raiden_Updates"> <img src="https://img.shields.io/badge/Update-Channel-green?style=for-the-badge&logo=telegram" alt="Update Channel" /> </a>
+</p>
---
+++ config.py
@@ -0,0 +1,118 @@
+from pyrogram import filters
+
+class config:
+    
+    #Client
+    API_ID = 28620311
+    API_HASH = "3b5c4ed0598e48fc1ab552675555e693"
+    BOT_TOKEN = "7797817336:AAGSE1E9EyRCvJhmJLlic_en-eo3IIAkiU4"
+    BOT_NAME = "Naruto Uzumaki"
+    BOT_USERNAME = "@Naruto_RoXbot"
+    BOT_ID = "8001037032"
+    WORKERS = 30 #Don't Change 
+    MAX_MESSAGE_CACHE_SIZE = 100 #Don't Change 
+    MAX_CONCURRENT_TRANSMISSIONS = 10 #Don't Change 
+    SESSION_STRING = ""
+    
+     
+    #Git
+    GIT_USERNAME = "Sungjinw04"
+    GIT_URL_WITH_TOKEN = "https://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@github.com/Sungjinw04/Mikasa.git" 
+
+
+    #Info
+    BOT_VERSION = "1.1"
+    OWNER_ID = 7392339658
+    DEV_ID = 7637698136
+    JOHAN = 7668725569
+    OWNERS = [OWNER_ID, JOHAN]
+    JOHAN_USERNAME = "shenarc"
+    SUPPORT_CHAT = -1001968443561
+    SUPPORT_CHAT_USERNAME = "ShinobiXupdates"
+    SUPPORT_CHAT_LINK = "https://t.me/shinobiXupdates"
+    LOG_CHANNEL = -1002359614394
+    ERROR_LOG_CHANNEL = -1002359614394
+    DOWNLOAD_LOCATION = "./downloads"
+    COMMAND_PREFIXES = ["/" , "!" , "." , "#" , "$" , "%" , "&" , "?"] 
+    CMD_STARTERS = "/.!&#%$"
+    STATS_IMG_URL = "https://files.catbox.moe/5swfse.jpg"
+    START_IMG_URL = "https://files.catbox.moe/fky73m.jpg"
+    HELP_IMG_URL = "https://files.catbox.moe/5swfse.jpg"
+    ALIVE_IMG_URL = "https://files.catbox.moe/fky73m.jpg"
+    
+    
+    #Database
+    MONGODB_URI = "mongodb://localhost:27017"
+    DATABASE_NAME = "Kafkaaa"
+
+    #API
+    ARQ_API_KEY = "RLWCED-WZASYO-AWOLTB-ITBWTP-ARQ"
+    ARQ_API_URL = "arq.hamker.dev"
+    CRICKET_API_URL = "https://sugoi-api.vercel.app/cricket"
+    FOOTBALL_API_URL = "https://sugoi-api.vercel.app/football"
+    BINGSEARCH_URL = "https://sugoi-api.vercel.app/search"
+    NEWS_URL = "https://sugoi-api.vercel.app/news?keyword={}"
+    shayri_api_url = "https://hindi-quotes.vercel.app/random"
+    BASE_URL = "https://api.waifu.pics"
+    OPENAI_KEY = "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"
+    Movie_Api = "5d3274c3bb08b4276482436c8444abc0"
+    Movie_RAC = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZDMyNzRjM2JiMDhiNDI3NjQ4MjQzNmM4NDQ0YWJjMCIsIm5iZiI6MTczMzIyMjgxMy42OTQwMDAyLCJzdWIiOiI2NzRlZTE5ZDJjZTRjZTdkZDYwOTU2YjAiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.aXfQw0_CRrKl2iSJd9tFE1TVbWWVYNgysWkUVlwzyRg"
+    Pokedex = "https://sugoi-api.vercel.app/pokemon?name={name_or_id}"
+    OPENAI_KEY = "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"
+    LYRICS_GENIUS_TOKEN = "aUxVC27LbOxlX4e2tGU7K_dXcy895BvxaCZol4uBHS_Ul4g_W5ndQsr-4NFbMwg5"
+    PASTEBIN_API = "Yt66ZvmHZoDIXsstRytMqzXNnMdKkf7m"
+
+
+
+#============================================================  M  U  S  I  C  ============================================================#
+
+
+API_ID = config.API_ID
+API_HASH = config.API_HASH
+BOT_TOKEN = config.BOT_TOKEN
+MONGO_DB_URI = config.MONGODB_URI
+DURATION_LIMIT_MIN = 600000000000000
+LOG_GROUP_ID = -1002274342742
+OWNER_ID = config.OWNER_ID
+HEROKU_APP_NAME = "HEROKU_APP_NAME"
+HEROKU_API_KEY = "HEROKU_API_KEY"
+UPSTREAM_REPO = "https://github.com/AnonymousX1025/AnonXMusic"
+UPSTREAM_BRANCH = "master"
+GIT_TOKEN = None
+SUPPORT_CHANNEL = config.SUPPORT_CHAT_LINK
+SUPPORT_GROUP = config.SUPPORT_CHAT_LINK
+AUTO_LEAVING_ASSISTANT = False
+SPOTIFY_CLIENT_ID = "6bf7d415e88b4225902363f8ee48cbdd"
+SPOTIFY_CLIENT_SECRET = "5dc9099056c14d63a9861803f4b2a4e0"
+PLAYLIST_FETCH_LIMIT = 100000000
+TG_AUDIO_FILESIZE_LIMIT =  50485760000
+TG_VIDEO_FILESIZE_LIMIT = 507374182400
+STRING1 = config.SESSION_STRING
+STRING2 = None
+STRING3 = None
+STRING4 = None
+STRING5 = None
+BANNED_USERS = filters.user()
+adminlist = {}
+lyrical = {}
+votemode = {}
+autoclean = []
+confirmer = {}
+START_IMG_URL = config.START_IMG_URL
+PING_IMG_URL = "https://envs.sh/Na0.jpg"
+PLAYLIST_IMG_URL = "https://envs.sh/Naq.jpg"
+STATS_IMG_URL = config.STATS_IMG_URL
+TELEGRAM_AUDIO_URL = "https://envs.sh/Naw.jpg"
+TELEGRAM_VIDEO_URL = "https://envs.sh/NaP.jpg"
+STREAM_IMG_URL = "https://envs.sh/Naw.jpg"
+SOUNCLOUD_IMG_URL = "https://envs.sh/Naq.jpg"
+YOUTUBE_IMG_URL = "https://envs.sh/Naq.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://envs.sh/Na0.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://envs.sh/Naq.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://envs.sh/Naw.jpg"
+
+def time_to_seconds(time):
+    stringt = str(time)
+    return sum(int(x) * 60**i for i, x in enumerate(reversed(stringt.split(":"))))
+
+DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
